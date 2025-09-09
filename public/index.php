759739f6f5b0b766a3cc1984ca9f708618a072ce
Ruchini <ruchinisenarathna20@gmail.com>
2025-09-09 17:15:54 +0530

    counselor Frontend

--- public/index.php
+++ public/index.php
@@ -12,4 +12,11 @@ $router = new Router();
 $router->get('/ug', 'UGControl@index');
 $router->get('/ug/appointment', 'UGControl@appointment');
 
+$router->get('/counselor', 'COControl@index');  
+$router->get('/counselor/dashboard', 'COControl@dashboard');
+$router->get('/counselor/appointments', 'COControl@appointmentmgt');
+$router->get('/counselor/calender', 'COControl@calender');
+$router->get('/counselor/session-history', 'COControl@sessionHistory');
+
+
 $router->dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
